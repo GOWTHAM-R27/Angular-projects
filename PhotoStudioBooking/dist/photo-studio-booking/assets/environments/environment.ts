2519caca0bb1bf9987dcac93cfbe372c3ec915db
GOWTHAM-R27 <111739347+GOWTHAM-R27@users.noreply.github.com>
2023-12-15 12:16:31 +0530

    Add files via upload

---
+++ PhotoStudioBooking/dist/photo-studio-booking/assets/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  registerUrl: 'http://localhost:3000/users',
+  bookingUrl:'http://localhost:3000/Booking',
+  priceUrl:'http://localhost:3000/Prices',
+  basicDiscountUrl:'http://localhost:3000/DiscountPrice/1',
+  premiumDiscountUrl:'http://localhost:3000/DiscountPricePremium/1',
+  aboutUsUrl:'http://localhost:3000/About/1',
+  contactEmailUrl:'http://localhost:3000/ContactsEmail/1',
+  contactMobileUrl:'http://localhost:3000/ContactsMobile/1',
+  contactAddressUrl:'http://localhost:3000/ContactsAddress/1',
+  feedbackUrl:'http://localhost:3000/Feedback'
+
+};
